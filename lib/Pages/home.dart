fff1e704d42afbebb1ff6df851d6cbfb420247b4
ansotomUtalca <alsoto17@alumnos.utalca.cl>
2024-04-25 23:47:42 -0400

    logica Laboratorio 5
    Icons for Defeat and Victory, text for Defeat and Victory. Logic included to change the text and icon onscreen.

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -2,19 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 const String assetname = 'lib/Icons/agregar-usuario.svg';
-Widget svgIcon = SvgPicture.asset(assetname, semanticsLabel: 'Logo');
+Widget svgIcon =
+    SvgPicture.asset(assetname, semanticsLabel: 'Logo', height: 24, width: 24);
+const String asset1 = 'lib/Icons/victoria.svg';
+const String asset2 = 'lib/Icons/derrota.svg';
+Widget svgVictory =
+    SvgPicture.asset(asset1, semanticsLabel: 'Icon', height: 24, width: 24);
+Widget svgDefeat =
+    SvgPicture.asset(asset2, semanticsLabel: 'Icon', height: 24, width: 24);
+const String asset3 = 'lib/Icons/pause.svg';
+Widget svgPause =
+    SvgPicture.asset(asset3, semanticsLabel: 'Icon', height: 24, width: 24);
+
+String victoria = 'Felicitaciones!';
+String derrota = 'Mala suerte.';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -27,13 +31,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
+    //increases counter and calls build method
   }
 
   void _decreaseCounter() {
@@ -50,59 +50,48 @@ class _MyHomePageState extends State<MyHomePage> {
     //resets counter back to 0 and calls build method.
   }
 
+  String _checkVictory() {
+    if (_counter == 15)
+      return derrota;
+    else if (_counter == 10)
+      return victoria;
+    else
+      return 'Juego terminado';
+  }
+
+  Widget _checkIconVictory() {
+    if (_counter == 15)
+      return svgDefeat;
+    else if (_counter == 10)
+      return svgVictory;
+    else
+      return svgPause;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Has apretado el boton esta cantidad de veces:',
-            ),
+            _checkIconVictory(),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Text(
+              _checkVictory(),
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: _resetCounter,
-          tooltip: 'Reset',
-          child:
-              svgIcon), // This trailing comma makes auto-formatting nicer for build methods.
+      //floatingActionButton: FloatingActionButton(
+      //onPressed: _resetCounter, tooltip: 'Reset', child: svgIcon),
       persistentFooterButtons: [
         FloatingActionButton(
             onPressed: _incrementCounter,
